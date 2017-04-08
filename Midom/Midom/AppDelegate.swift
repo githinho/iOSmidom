7ed32b90e25a3a89f8a4bd5a1393a8b19c71b01c
Eugen Druzin <eugen.druzin@ingemark.com>
2017-04-08 16:09:29 +0200

    create Xcode project

---
+++ Midom/Midom/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  Midom
+//
+//  Created by Eugen Druzin on 08/04/2017.
+//  Copyright © 2017 ne. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let viewController = ViewController()
+        let nc = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = nc
+        window?.makeKeyAndVisible()
+        
+        window?.backgroundColor = .white
+        
+        return true
+    }
+
+}
+
