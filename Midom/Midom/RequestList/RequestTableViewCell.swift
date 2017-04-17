846277f8372e79847d68bb0dad849376149e4c57
Eugen Druzin <eugen.druzin@ingemark.com>
2017-04-17 21:24:52 +0200

    add request cell

---
+++ Midom/Midom/RequestList/RequestTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  RequestTableViewCell.swift
+//  Midom
+//
+//  Created by Eugen Druzin on 17/04/2017.
+//  Copyright © 2017 ne. All rights reserved.
+//
+
+import UIKit
+
+class RequestTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var nameStudyLabel: UILabel!
+    @IBOutlet weak var specialistLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    public func configure() {
+        
+    }
+    
+}
