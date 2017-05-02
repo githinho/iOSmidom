018429ad70fbcfd87efa33dce938bf13dda2576d
Eugen Druzin <eugen.druzin@ingemark.com>
2017-05-02 22:04:12 +0200

    add Compose VC to enable user sending comment on Consultation request and closing it

---
+++ Midom/Midom/AcceptedRequest/ComposeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ComposeViewController.swift
+//  Midom
+//
+//  Created by Eugen Druzin on 02/05/2017.
+//  Copyright © 2017 ne. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var commentTextView: UITextView!
+    
+    private let service: MidomService
+    private let crId: Int
+    
+    init(midomService: MidomService, crId: Int) {
+        self.service = midomService
+        self.crId = crId
+        super.init(nibName: "ComposeViewController", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func sendCommentButtonClicked(_ sender: UIButton) {
+        if let comment = commentTextView.text {
+            // TODO: send to server comment
+            
+        } else {
+            self.view.makeToast("Cannot send empty comment")
+        }
+    }
+}
