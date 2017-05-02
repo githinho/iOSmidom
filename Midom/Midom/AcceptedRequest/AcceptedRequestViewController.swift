a9d575663699e93ec6875323834e8a6029f18f1d
Eugen Druzin <eugen.druzin@ingemark.com>
2017-05-02 20:34:46 +0200

    add Accepted Request Controller to show accepted request with image collection view

---
+++ Midom/Midom/AcceptedRequest/AcceptedRequestViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AcceptedRequestViewController.swift
+//  Midom
+//
+//  Created by Eugen Druzin on 02/05/2017.
+//  Copyright © 2017 ne. All rights reserved.
+//
+
+import UIKit
+
+class AcceptedRequestViewController: UIViewController {
+
+    @IBOutlet weak var studyProviderLabel: UILabel!
+    @IBOutlet weak var studyNameLabel: UILabel!
+    @IBOutlet weak var commentLable: UILabel!
+    @IBOutlet weak var imageCollectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
